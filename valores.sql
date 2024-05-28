603e54c415fdd527de22cc343f972208a7400798
Bernardo Bones <bonesb342@gmail.com>
2024-05-28 20:27:39 -0300

    create sql

---
+++ valores.sql
@@ -0,0 +1,34 @@
+INSERT INTO artista (nome, genero) VALUES
+('Avenged Sevenfold', 'Metal')
+
+INSERT INTO album (titulo, ano_lancamento, id_artista) VALUES
+('Sounding the Seventh Trumpet', 2001, 1),
+('Waking the Fallen', 2003, 1),
+('City of Evil', 2005, 1),
+('Avenged Sevenfold', 2007, 1),
+('Nightmare', 2010, 1),
+('Hail to the King', 2013, 1),
+('The Stage', 2016, 1),
+('Life Is But a Dream...', 2023, 1);
+
+INSERT INTO musica (titulo, duracao, id_album) VALUES
+('To End the Rapture', 108, 1),
+('Turn the Other Way', 298, 1),
+('Unholy Confessions', 267, 2),
+('Second Heartbeat', 443, 2),
+('Bat Country', 311, 3),
+('Seize the Day', 328, 3),
+('Almost Easy', 259, 4),
+('Afterlife', 350, 4),
+('Nightmare', 377, 5),
+('So Far Away', 300, 5),
+('Hail to the King', 338, 6),
+('Shepherd of Fire', 314, 6),
+('The Stage', 511, 7),
+('God Damn', 243, 7),
+('Game Over', 247, 8),
+('Nobody', 280, 8),
+('Eternal Rest', 306, 1),
+('Chapter Four', 347, 2),
+('Beast and the Harlot', 342, 3),
+('Critical Acclaim', 301, 4);
